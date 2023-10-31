8514a8c70c74edda7778b935fbd0b8610973abca
Filip Zagozdon <filipzagozdon8@gmail.com>
2023-11-01 00:42:39 +0100

    car-fleet: Improved O(n) time solution, where n is target

--- car-fleet/Solution.java
+++ car-fleet/Solution.java
@@ -1,31 +1,19 @@
 class Solution {
-    public record PositionSpeedPair(int position, int speed) {}
     public int carFleet(int target, int[] position, int[] speed) {
-        List<PositionSpeedPair> positionSpeedPairs = createListOfPositionSpeedPairs(position, speed);
-        Comparator<PositionSpeedPair> positionReverseComparator = Comparator
-                .comparing(PositionSpeedPair::position, Comparator.reverseOrder());
-        positionSpeedPairs.sort(positionReverseComparator);
+        float[] timesLeftIndexedByPosition = new float[target];
+        for (int i = 0; i < position.length; i++) {
+            timesLeftIndexedByPosition[position[i]] = (target - position[i]) / (float) speed[i];
+        }
 
-        List<Double> flockArrivalTimes = new ArrayList<>();
-        
-        for (PositionSpeedPair pair : positionSpeedPairs) {
-            double arrivalTime = (target - pair.position) / (double)pair.speed;
-            flockArrivalTimes.add(arrivalTime);
-            if (flockArrivalTimes.size() >= 2 && flockArrivalTimes.get(flockArrivalTimes.size() - 1) <= flockArrivalTimes.get(flockArrivalTimes.size() - 2)) {
-                flockArrivalTimes.remove(flockArrivalTimes.size() - 1);
+        int distinctFlocksCounter = 0;
+        float furtherFlockArrivalTime = 0;
+        for (int i = target - 1; i >= 0; i--) {
+            if (timesLeftIndexedByPosition[i] > furtherFlockArrivalTime) {
+                distinctFlocksCounter++;
+                furtherFlockArrivalTime = timesLeftIndexedByPosition[i];
             }
         }
 
-        return flockArrivalTimes.size();
-    }
-
-    private List<PositionSpeedPair> createListOfPositionSpeedPairs(int[] position, int[] speed) {
-        List<PositionSpeedPair> result = new ArrayList<>();
-        
-        for (int i = 0; i < position.length; i++) {
-            result.add(new PositionSpeedPair(position[i], speed[i]));
-        }
-
-        return result;
+        return distinctFlocksCounter;
     }
 }
